90de045c001cd362a090c6462f27693b36fba2ac
Daniel Linker <daniel.linker@intellicode-solutions.de>
2025-08-10 18:34:32 +0200

    Initial commit

---
+++ apps/users/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+@Component({
+  imports: [  CommonModule,
+    RouterModule,
+    MatCardModule,  // MatCardModule für mat-card-Komponenten
+    MatButtonModule
+  ],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'users';
+}
